44d0741d8f9a9a7e9aa75430a1500d8650119028
kalni <kalnichenko.1996@mail.ru>
2023-01-13 22:46:07 +0300

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,9 @@
 import {fileURLToPath} from "node:url";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    buildDir: '.output/nuxt3-ci-cd-example/',
-    // nitro: {
-    //     output: { dir: '/nuxt3-ci-cd-example/'}
-    // },
-    // alias: {
-    //     'images': fileURLToPath(new URL('./assets/432/images', import.meta.url)),
-    //     'style': fileURLToPath(new URL('./assets/style', import.meta.url)),
-    //     'assets': fileURLToPath(new URL('./assets/dasdsasad/data', import.meta.url))
-    // },
+    router: {
+        base: '/nuxt3-ci-cd-example/'
+    },
     vite: {
         css: {
             preprocessorOptions: {
